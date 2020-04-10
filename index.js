11f6e4636faa4aee60af2954495d218a9c54aa8f
John Martin <jrmartin182@gmail.com>
2020-04-10 13:15:11 -0600

    linked resume

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let welcome_container = document.getElementById('welcome');
+const welcome_container = document.getElementById('welcome');
+const collapseUpBtn = document.getElementById('collapse_up');
+const collapseDownBtn = document.getElementById('collapse_down');
 
 
 function hideWelcome(){
+    console.log("hidden");
     welcome_container.style.display = 'none';
-}
+
+    if(collapseUpBtn.style.display = ''){
+        collapseDownBtn.style.display = 'none';
+    } else {
+        collapseUpBtn.style.display = '';
+    }
+};
+
+collapseUpBtn.addEventListener('click', hideWelcome);
+collapseDownBtn.addEventListener('click', hideWelcome);
+
+
